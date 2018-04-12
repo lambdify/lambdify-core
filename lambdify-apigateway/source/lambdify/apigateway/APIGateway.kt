bc09afc22fb9c9f49575191800578e85e065bf62
Miere Teixeira <miere00@gmail.com>
2018-04-12 14:34:48 -0300

    Updated APIGateway API according to https://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-mapping-template-reference.html#context-variable-reference

--- lambdify-apigateway/source/lambdify/apigateway/APIGateway.kt
+++ lambdify-apigateway/source/lambdify/apigateway/APIGateway.kt
@@ -1,7 +1,5 @@
 package lambdify.apigateway
 
-import kotlin.reflect.KClass
-
 /**
  * Created by miere.teixeira on 06/04/2018.
  */
@@ -9,35 +7,39 @@ class APIGatewayRequest {
     var resource: String? = null
     var path: String = ""
     var httpMethod: String? = null
-    var headers: Map<String, String>? = null
-    var queryStringParameters: Map<String, String>? = null
-    var pathParameters: Map<String, String>? = null
-    var stageVariables: Map<String, String>? = null
+    var headers: Map<String, String>? = emptyMap()
+    var queryStringParameters: Map<String, String>? = emptyMap()
+    var pathParameters: Map<String, String>? = emptyMap()
+    var stageVariables: Map<String, String>? = emptyMap()
     var requestContext: APIGatewayRequestContext? = null
     var body: String? = null
     var isBase64Encoded: Boolean = false
 }
 
 class APIGatewayRequestContext {
+    var apiId: String? = null
     var accountId: String? = null
+    var authorizer:Map<String, String>? = emptyMap()
+    var error:Map<String, String>? = emptyMap()
+    var identity: APIGatewayRequestContextIdentity? = null
+    var path:String? = null
     var resourceId: String? = null
     var stage: String? = null
     var requestId: String? = null
-    var identity: APIGatewayRequestContextIdentity? = null
     var resourcePath: String? = null
     var httpMethod: String? = null
-    var apiId: String? = null
 }
 
 class APIGatewayRequestContextIdentity {
-    var cognitoIdentityPoolId: String? = null
     var accountId: String? = null
-    var cognitoIdentityId: String? = null
-    var caller: String? = null
     var apiKey: String? = null
-    var sourceIp: String? = null
+    var apiKeyId:String? = null
+    var caller: String? = null
+    var cognitoIdentityPoolId: String? = null
+    var cognitoIdentityId: String? = null
     var cognitoAuthenticationType: String? = null
     var cognitoAuthenticationProvider: String? = null
+    var sourceIp: String? = null
     var userArn: String? = null
     var userAgent: String? = null
     var user: String? = null
